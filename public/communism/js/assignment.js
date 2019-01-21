3c7575125188b27e8065e0f57d36c9889f739412
Martin Jonsson <martinjonsson01@gmail.com>
2019-01-21 17:40:38 +0100

    Finalized texts

--- public/communism/js/assignment.js
+++ public/communism/js/assignment.js
@@ -61,3 +61,20 @@ function updateDynamicHeaders() {
     // Update last edited header.
     updateLastEditedHeader();
 }
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topButton").style.display = "block";
+  } else {
+    document.getElementById("topButton").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
